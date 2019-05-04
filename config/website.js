a1558d0074803d9c161b18aaf18ef1b1f89f88a3
Taylor MacDonald <tcmacdonald@gmail.com>
2019-05-04 11:54:47 -0400

    Update website.js

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Starter - Prismic.io', // Navigation and Site Title
-  titleAlt: 'Gatsby Prismic.io', // Title for JSONLD
-  description: 'A typography-heavy & light-themed Gatsby Starter which uses the Headless CMS Prismic.',
-  headline: 'Writing and publishing content for LekoArts', // Headline for schema.org JSONLD
-  url: 'https://prismic.lekoarts.de', // Domain of your site. No trailing slash!
+  title: 'Crimson MacDonald, Political Consultant', // Navigation and Site Title
+  titleAlt: 'Crimson MacDonald, LLC', // Title for JSONLD
+  description: 'Biography and thoughts of Crimson MacDonald.',
+  headline: 'Biography and thoughts of Crimson MacDonald', // Headline for schema.org JSONLD
+  url: 'https://crimsonmac.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/logo-1024.png', // Used for SEO
   ogLanguage: 'en_US', // Facebook Language
 
   // JSONLD / Manifest
   favicon: 'src/favicon.png', // Used for manifest favicon generation
-  shortName: 'Prismic', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  shortName: 'CrimsonMac', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Crimson MacDonald', // Author for schemaORGJSONLD
   themeColor: '#3D63AE',
   backgroundColor: '#EBEDF2',
 
